b7794f3e3b51bcad92f9091a4836f329cc37c3f8
316de <316de@315-DE-13>
2023-07-05 10:39:54 +0300

    Добавьте файлы проекта.

---
+++ ClassLibraryK/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassLibraryK
+{
+	public class Person
+	{
+		// книги данного человека
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public List<Hobby> Hobby = new List<Hobby>();
+		public Person(string nm, string ad)
+		{
+			Name = nm;
+			Address = ad;
+		}
+		public void ShowAll()
+		{
+			Console.WriteLine($"Человек: имя - {Name}, адрес - {Address}");
+			Console.WriteLine("Список любимых занятий:");
+			Hobby.Sort();
+			foreach (Hobby hobby in Hobby) 
+			{
+				hobby.Show(); 
+			}
+		}
+
+	}
+}
