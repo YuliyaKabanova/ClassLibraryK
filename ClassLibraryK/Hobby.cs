aeea9c91118236133b0fb86bb7d805900436e972
is24kabanovaya@outlook.com <is24kabanovaya@outlook.com>
2023-07-05 10:43:47 +0300

    Создание интерфейса

--- ClassLibraryK/Hobby.cs
+++ ClassLibraryK/Hobby.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace ClassLibraryK
 {
+	//Кабанова Ю.А. 31ИС
+	//Класс - хобби
 	internal class Hobby
 	{
+		public class hobby : IComparable<Hobby>
+		{
+			public string Name { get; set; }
+			public string Description { get; set; }
+			public hobby(string nm, string descr)
+			{
+				Name = nm;
+				Description = descr;
+			}
+			public void Show()
+			{
+				Console.WriteLine(
+					 $"Название: {Name}, Описание - {Description}");
+			}
+			public int CompareTo(Hobby? other)
+			{
+				return Name.CompareTo(other.Name);
+			}
+		}
 	}
 }
+
